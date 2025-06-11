0a838c22e25687df304f2a526e8b9fae10864557
lky062@naver.com <lky062@naver.com>
2025-06-12 01:09:39 +0900

    feat: svg 스크롤 애니메이션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,37 +6,45 @@ import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import FeaturedWork from '@/components/FeaturedWork';
 import Header from '@/components/Header';
+import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin);
 
 export default function Home() {
   const container = useRef<HTMLElement | null>(null);
   const textRef = useRef<HTMLDivElement | null>(null);
   const pathRef = useRef<SVGPathElement | null>(null);
+  const vector6Ref = useRef<SVGPathElement>(null);
+  const vector7Ref = useRef<SVGPathElement>(null);
 
   useGSAP(
     () => {
-      gsap.fromTo(textRef.current, { opacity: 0, y: 50 }, { opacity: 1, y: 0, duration: 1, ease: 'power3.out' });
-
-      if (pathRef.current) {
-        const pathLength = pathRef.current.getTotalLength();
-
-        gsap.set(pathRef.current, {
-          strokeDasharray: pathLength,
-          strokeDashoffset: pathLength,
-        });
-
-        gsap.to(pathRef.current, {
-          strokeDashoffset: 0,
-          ease: 'none',
-          scrollTrigger: {
-            trigger: container.current,
-            start: 'top center',
-            end: 'bottom center',
-            scrub: true,
-          },
-        });
+      // 텍스트 페이드인 애니메이션
+      if (textRef.current) {
+        gsap.fromTo(textRef.current, { opacity: 0, y: 50 }, { opacity: 1, y: 0, duration: 1, ease: 'power3.out' });
       }
+
+      const animatePath = (path: SVGPathElement | null) => {
+        if (!path) return;
+
+        gsap.fromTo(
+          path,
+          { drawSVG: '0%' },
+          {
+            drawSVG: '100%',
+            ease: 'none',
+            scrollTrigger: {
+              trigger: container.current,
+              start: 'top center',
+              end: 'bottom center',
+              scrub: true,
+            },
+          },
+        );
+      };
+
+      animatePath(vector6Ref.current);
+      animatePath(vector7Ref.current);
     },
     { scope: container },
   );
@@ -46,21 +54,44 @@ export default function Home() {
       ref={container}
       className="h-[200vh] bg-[#E8E9F1] flex flex-col items-start relative overflow-hidden px-8 pt-24">
       <Header />
-      <svg viewBox="0 0 1440 1024" className="absolute top-0 left-0 w-full h-full z-0">
+      {/* vector 6 */}
+      {/* TODO: 애니메이션 사이즈 및 크기 조절 */}
+      <svg
+        width="30%"
+        viewBox="0 0 496 330"
+        className="absolute top-0 left-0"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
         <path
-          ref={pathRef}
-          d="M1308 112C959.619 68.589 747.5 188 561.5 358C371.964 530.357 271.612 723.947 114.5 912.5C91.8709 940.316 22.2186 987.539 10 910.5C1.17811 854.706 36.8925 768.012 114.5 752C218.426 730.517 365.346 830.093 488.5 896C711.658 1015.09 959.619 1228.25 1278 1024"
-          stroke="#3D43F0"
-          strokeWidth="20"
-          fill="none"
+          ref={vector6Ref}
+          d="M-20.5913 183.44C126.033 140.6 136.494 301.372 89.2738 309.357C25.4699 320.146 59.5532 150.202 203.378 61.6678C316.027 -7.6749 404.017 13.9683 480.169 107.645"
+          stroke="#3B48F4"
+          strokeWidth="40"
         />
       </svg>
 
-      <div className="z-10 max-w-5xl w-full flex flex-col md:flex-row items-center md:justify-start gap-16 mx-auto relative">
+      {/* vector 7 */}
+      <svg
+        width="50%"
+        viewBox="0 0 986 534"
+        className="absolute top-150 right-0"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          ref={vector7Ref}
+          d="M34.489 5.30199C-9.98855 173.836 45.3868 293.77 235.938 363.483C479.225 452.49 675.806 311.091 603 247C549.117 199.567 366.44 338.804 556 470.5C698.667 574 1089.2 456.5 1116 458.5"
+          stroke="#3B48F4"
+          strokeWidth="40"
+        />
+      </svg>
+
+      <div className="z-10 max-w-5xl w-full md:flex-row items-center md:justify-start gap-16 mx-auto relative mt-5">
         <div className="text-black" ref={textRef}>
           <h1 className="text-6xl font-extrabold mb-8 leading-tight">
             Beyond Code <br /> Toward Better Lives
           </h1>
+        </div>
+        <div className="md:w-1/2 md:ml-[50%]">
           <p className="text-lg font-semibold leading-relaxed mb-6 max-w-lg">
             I am a human-centered front-end developer. Through development, I strive to ensure that no one is left
             behind by technology, creating web experiences that make everyone's lives a little better. Beyond
