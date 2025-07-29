2c92d54ec2833a965debbf56b03586e1e98d9de4
lky062@naver.com <lky062@naver.com>
2025-07-29 19:22:58 +0900

    chore: 커리어 타임라인 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,6 @@ import TimelineAnimation from '@/components/TimelineAnimation';
 import ArticleList from '@/components/ArticleList';
 import { useArticles } from '@/hooks/useArticles';
 import ParticleBackground from '@/components/ParticleBackground';
-import CareerTimeline from '@/components/CareerTimeline';
 
 gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin);
 
